1dc23c3dfb15ff5239f2aea6855e824fdcd2439a
Abhishek Shukla <4747uwu@gmail.com>
2025-10-07 14:43:31 +0530

    dfd

--- MedicalProject/backend/config/db.js
+++ MedicalProject/backend/config/db.js
@@ -7,7 +7,7 @@ dotenv.config();
 const connectDB = async () => {
     try {
         console.log('🔗 Connecting to MongoDB replica set...');
-        const MONGODB_URI ='mongodb://meduser:SecurePass_2025@localhost:27017/?authSource=admin&replicaSet=rs0'
+        const MONGODB_URI ='mongodb://meduser:SecurePass_2025@mongo-rs:27017/?authSource=admin&replicaSet=rs0'
         console.log(`🌐 Using MONGODB_URI: ${MONGODB_URI}`);
 
         
